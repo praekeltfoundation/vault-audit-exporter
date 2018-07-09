712f35f24aed33a818a73f95f6c5451b3fff2eac
Jamie Hewland <jhewland@gmail.com>
2018-07-09 14:59:03 +0200

    Silly text editor didn't do tabs

--- server_test.go
+++ server_test.go
@@ -1,43 +1,43 @@
 package vaultAuditExporter
 
 import (
-  "encoding/json"
-  "net"
+	"encoding/json"
+	"net"
 )
 
 func setupHander(ts *TestSuite) (net.Conn, *AuditEntryQueue) {
-  server, client := net.Pipe()
-  ts.AddCleanup(func(){ ts.Nil(server.Close()) })
+	server, client := net.Pipe()
+	ts.AddCleanup(func() { ts.Nil(server.Close()) })
 
-  queue := NewAuditEntryQueue()
-  ts.AddCleanup(queue.Close)
+	queue := NewAuditEntryQueue()
+	ts.AddCleanup(queue.Close)
 
-  go handleConnection(client, queue)
+	go handleConnection(client, queue)
 
-  return server, queue
+	return server, queue
 }
 
 func writeJSONLine(ts *TestSuite, v interface{}, conn net.Conn) {
-  b, _ := ts.WithoutError(json.Marshal(v)).([]byte)
+	b, _ := ts.WithoutError(json.Marshal(v)).([]byte)
 
-  ts.WithoutError(conn.Write(b))
-  ts.WithoutError(conn.Write([]byte("\n")))
+	ts.WithoutError(conn.Write(b))
+	ts.WithoutError(conn.Write([]byte("\n")))
 }
 
 func (ts *TestSuite) TestHandleRequest() {
-  server, queue := setupHander(ts)
+	server, queue := setupHander(ts)
 
-  req := dummyRequest()
-  writeJSONLine(ts, req, server)
+	req := dummyRequest()
+	writeJSONLine(ts, req, server)
 
-  ts.Equal(req, <-queue.Receive())
+	ts.Equal(req, <-queue.Receive())
 }
 
 func (ts *TestSuite) TestHandleResponse() {
-  server, queue := setupHander(ts)
+	server, queue := setupHander(ts)
 
-  res := dummyResponse()
-  writeJSONLine(ts, res, server)
+	res := dummyResponse()
+	writeJSONLine(ts, res, server)
 
-  ts.Equal(res, <-queue.Receive())
+	ts.Equal(res, <-queue.Receive())
 }
