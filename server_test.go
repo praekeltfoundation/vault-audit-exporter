40c3851b48b7f463616cc3854a21a3732d917bbf
Jamie Hewland <jhewland@gmail.com>
2018-07-11 16:30:30 +0200

    server: Clean up helpers and implement under own suite

--- server_test.go
+++ server_test.go
@@ -4,9 +4,41 @@ import (
 	"encoding/json"
 	"io"
 	"net"
+	"testing"
 
 	"github.com/hashicorp/vault/audit"
+	"github.com/stretchr/testify/suite"
 )
+
+// See helper_for_test.go for common infrastructure and tools.
+
+// ServerTests is a testify test suite object that we can attach helper methods
+// to.
+type ServerTests struct{ TestSuite }
+
+// TestServer is a standard Go test function that runs our test suite's tests.
+func TestServer(t *testing.T) { suite.Run(t, new(ServerTests)) }
+
+func (ts *ServerTests) writeJSONLine(writer io.Writer, v interface{}) {
+	b, err := json.Marshal(v)
+	ts.NoError(err)
+	ts.writeLine(writer, b)
+}
+
+func (ts *ServerTests) writeStringLine(writer io.Writer, line string) {
+	ts.write(writer, []byte(line), []byte{'\n'})
+}
+
+func (ts *ServerTests) writeLine(writer io.Writer, line []byte) {
+	ts.write(writer, line, []byte{'\n'})
+}
+
+func (ts *ServerTests) write(writer io.Writer, bytes ...[]byte) {
+	for _, b := range bytes {
+		_, err := writer.Write(b)
+		ts.NoError(err)
+	}
+}
 
 type auditEntryCollector struct {
 	requests  []*audit.AuditRequestEntry
@@ -28,27 +60,13 @@ func (col *auditEntryCollector) HandleResponse(res *audit.AuditResponseEntry) {
 	col.responses = append(col.responses, res)
 }
 
-func writeJSONLine(v interface{}, writer io.Writer) {
-	b, _ := json.Marshal(v)
-	writeLine(b, writer)
-}
-
-func writeStringLine(s string, writer io.Writer) {
-	writeLine([]byte(s), writer)
-}
-
-func writeLine(b []byte, writer io.Writer) {
-	_, _ = writer.Write(b)
-	_, _ = writer.Write([]byte{'\n'})
-}
-
-func (ts *TestSuite) TestHandleRequest() {
+func (ts *ServerTests) TestHandleRequest() {
 	server, client := net.Pipe()
 
 	req := dummyRequest()
 	go func() {
 		defer server.Close()
-		writeJSONLine(req, server)
+		ts.writeJSONLine(server, req)
 	}()
 
 	collector := newAuditEntryCollector()
@@ -58,13 +76,13 @@ func (ts *TestSuite) TestHandleRequest() {
 	ts.Equal(req, collector.requests[0])
 }
 
-func (ts *TestSuite) TestHandleResponse() {
+func (ts *ServerTests) TestHandleResponse() {
 	server, client := net.Pipe()
 
 	res := dummyResponse()
 	go func() {
 		defer server.Close()
-		writeJSONLine(res, server)
+		ts.writeJSONLine(server, res)
 	}()
 
 	collector := newAuditEntryCollector()
@@ -74,16 +92,16 @@ func (ts *TestSuite) TestHandleResponse() {
 	ts.Equal(res, collector.responses[0])
 }
 
-func (ts *TestSuite) TestUnknownTypeIgnored() {
+func (ts *ServerTests) TestUnknownTypeIgnored() {
 	server, client := net.Pipe()
 
 	req := dummyRequest()
 	res := dummyResponse()
 	go func() {
 		defer server.Close()
-		writeJSONLine(req, server)
-		writeStringLine("{\"type\": \"foo\"}", server)
-		writeJSONLine(res, server)
+		ts.writeJSONLine(server, req)
+		ts.writeStringLine(server, "{\"type\": \"foo\"}")
+		ts.writeJSONLine(server, res)
 	}()
 
 	collector := newAuditEntryCollector()
@@ -95,12 +113,12 @@ func (ts *TestSuite) TestUnknownTypeIgnored() {
 	ts.Equal(res, collector.responses[0])
 }
 
-func (ts *TestSuite) TestUnknownJSON() {
+func (ts *ServerTests) TestUnknownJSON() {
 	server, client := net.Pipe()
 	defer server.Close()
 
 	go func() {
-		writeStringLine("{\"foo\": \"bar\"}", server)
+		ts.writeStringLine(server, "{\"foo\": \"bar\"}")
 	}()
 
 	collector := newAuditEntryCollector()
@@ -110,12 +128,12 @@ func (ts *TestSuite) TestUnknownJSON() {
 	ts.Empty(collector.responses)
 }
 
-func (ts *TestSuite) TestInvalidJSON() {
+func (ts *ServerTests) TestInvalidJSON() {
 	server, client := net.Pipe()
 	defer server.Close()
 
 	go func() {
-		writeStringLine("baz", server)
+		ts.writeStringLine(server, "baz")
 	}()
 
 	collector := newAuditEntryCollector()
@@ -125,7 +143,7 @@ func (ts *TestSuite) TestInvalidJSON() {
 	ts.Empty(collector.responses)
 }
 
-func (ts *TestSuite) TestServe() {
+func (ts *ServerTests) TestServe() {
 	listener, _ := ts.WithoutError(net.Listen("tcp", "127.0.0.1:0")).(net.Listener)
 	ts.AddCleanup(func() { _ = listener.Close() })
 
@@ -148,8 +166,8 @@ func (ts *TestSuite) TestServe() {
 	res := dummyResponse()
 	go func() {
 		defer conn.Close()
-		writeJSONLine(req, conn)
-		writeJSONLine(res, conn)
+		ts.writeJSONLine(conn, req)
+		ts.writeJSONLine(conn, res)
 	}()
 
 	// Ensure they are received in the queue
